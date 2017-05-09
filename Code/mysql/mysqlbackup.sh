20d061dc6071afac44c1ee4cccc2fe0da91e63af
sunny5156 <137898350@qq.com>
2017-05-09 15:13:39 +0800

    整理修改 逻辑

--- Code/mysql/mysqlbackup.sh
+++ Code/mysql/mysqlbackup.sh
@@ -1,20 +1,18 @@
-#!/bin/sh
-#数据库备份
-# Database info
-DB_HOST="localhost"
-DB_NAME="db_gogs"
+#!/bin/bash
+
+#Database info
+DB_HOST="127.0.0.1"
+DB_NAME="db_cxnet"
 DB_USER="root"
 DB_PASS="a123654"
 
-# Others vars
-BCK_DIR="/data/Backup/"
+#Other vars
+BCK_DIR="/data/backup/mysql/"
 DATE=`date +%F`
 
 #mkdir dir
-#mkdir $BCK_DIR$DB_NAME -m 777
-#echo $BCK_DIR$DB_NAME
-#if[!-d "/data/Backup/db_gogs"];then
-#  mkdir $BCK_DIR$DB_NAME/ -m 777
-#fi
-# TODO
-mysqldump --opt -h$DB_HOST -u$DB_USER -p$DB_PASS $DB_NAME | gzip > $BCK_DIR/$DB_NAME-$DATE.gz
+if [ ! -d $BCK_DIR$DB_NAME ]; then
+    mkdir -p $BCK_DIR$DB_NAME -m 777
+fi
+mysqldump --opt -h$DB_HOST -u$DB_USER -p$DB_PASS $DB_NAME | gzip > $BCK_DIR$DB_NAME/$DB_NAME-$DATE.gz
+
