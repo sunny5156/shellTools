19d5bc05d7b4beaf183bcea7e818a8d45a1a69db
sunny5156 <sunny5156@qq.com>
2019-09-11 14:28:12 +0800

    edit

--- Code/mysql/mysql_optimzie.sh
+++ Code/mysql/mysql_optimzie.sh
@@ -10,13 +10,17 @@ do
 check_result=$(mysql -h$host_name -u$user_name -p$user_pwd $database -A -Bse "check table $table_name" | awk '{ print $4 }')
 if [ "$check_result" = "OK" ]
 then
-echo "It's no need to repair table $table_name"
+msg="It's no need to repair table $table_name"
+
+echo -e "\033[32m ${msg} \033[0m \n"
 else
-echo $(mysql -h$host_name -u$user_name -p$user_pwd $database -A -Bse "repair table $table_name")
+msg=$(mysql -h$host_name -u$user_name -p$user_pwd $database -A -Bse "repair table $table_name")
+echo -e "\033[31m ${msg} \033[0m \n"
 fi
 # 优化表,可提高性能
 if [ $need_optmize_table = true ]
 then
-echo $(mysql -h$host_name -u$user_name -p$user_pwd $database -A -Bse "optimize table $table_name")
+msg=$(mysql -h$host_name -u$user_name -p$user_pwd $database -A -Bse "optimize table $table_name")
+echo -e "\033[36m ${msg} \033[0m \n"
 fi
 done
